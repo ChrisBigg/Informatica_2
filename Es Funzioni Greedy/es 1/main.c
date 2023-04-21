7c41dbc7e925f2c271af127879add858aff7fe7c
ChrisBigg <chri.bignardi@gmail.com>
2023-04-21 10:06:42 +0200

    Salvataggio es monete
    non finito

---
+++ Es Funzioni Greedy/es 1/main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+extern int* Monete(const int* tagli, size_t tagli_size, int budget);
+
+int main(void) {
+
+
+	return EXIT_SUCCESS;
+}
+
+
---
+++ Es Funzioni Greedy/es 1/monete.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+
+void MoneteGr(const int* tagli, size_t tagli_size, int budget, int* ris, int i) {
+
+
+
+
+
+}
+
+
+
+
+
+int* Monete(const int* tagli, size_t tagli_size, int budget) {
+
+	int* ris = malloc(tagli_size * sizeof(int));
+	 
+	MoneteGr(tagli, tagli_size, budget, ris, 0);
+
+
+}
